4dd8e410fd0a08193d119fffa6b6f7984514dbbe
SREESANKAR-P <sreesankarputhumana@gmail.com>
2023-10-19 10:19:57 +0530

    removed my API key

--- src/api.jsx
+++ src/api.jsx
@@ -2,7 +2,7 @@
 export const geoApiOptions = {
     method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '4ffbb3d5b4msh53ff04382f162fep1ad556jsna9dcbdecf502',
+		'X-RapidAPI-Key': ' ',// enter your rapid api key here
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 
@@ -10,4 +10,4 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
-export const WEATHER_API_KEY = "74657d058e6a54f9f6ed47f95690474d";
+export const WEATHER_API_KEY = " ";// enter your key from openweather API
